797eec7d2d9a1b7d0e799930abb5704f23d3a3f5
farid miri <farid.miri.1393@gmail.com>
2024-04-22 21:22:20 +0300

    initialization

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/404.module.css'
+
+const NotFound = () => {
+  return (
+    <div className={`${styles.div}`}>
+       There is nothing to see here :)
+    </div>
+  );
+};
+
+export default NotFound;
